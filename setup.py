f5adbaa732134e8945479af4d795351aca01e4bc
Amarnath Reddy Kallam <61119710+Amarnath2501-Kallam@users.noreply.github.com>
2024-07-07 23:03:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as f:
+    readme_content = f.read()
+
+setup(
+    name="quality-prompts",
+    version="0.0.3",
+    packages=find_packages(),
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "litellm==1.41.8",
+    ],
+)
